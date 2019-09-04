6c34b0241f8f0c16c99633d4a3d07b54c49c24dd
sunq2018 <sunq22827@gmail.com>
2019-09-03 21:49:56 -0400

    敌方坦克加入

--- src/main/java/MIllerSun/tankwar/GameClient.java
+++ src/main/java/MIllerSun/tankwar/GameClient.java
@@ -4,19 +4,34 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
+import java.util.ArrayList;
+import java.util.List;
 
 public class GameClient extends JComponent {
 
     private Tank playerTank;
+    private List<Tank> enemyTanks;
+
+//    private List<Wall> wa1ls;
 
     public GameClient() {
         this.playerTank = new Tank(400,100,Direction.DOWN);
+        this.enemyTanks = new ArrayList<Tank>(12);
+        for (int i = 0; i < 3; i++) {
+            for(int j = 0; j < 4; j++){
+                this.enemyTanks.add(new Tank(240 + j * 100, 400 + i * 40, true, Direction.UP));
+            }
+        }
+
         this.setPreferredSize(new Dimension(800, 600));
     }
 
     @Override
     protected void paintComponent(Graphics g) {
         playerTank.draw(g);
+        for(Tank tank: enemyTanks) {
+            tank.draw(g);
+        }
     }
 
     public static void main(String[] args) {
--- src/main/java/MIllerSun/tankwar/Tank.java
+++ src/main/java/MIllerSun/tankwar/Tank.java
@@ -9,6 +9,7 @@ public class Tank {
 
     private int x;
     private int y;
+    private boolean enemy;
 
     public int getX() {
         return x;
@@ -29,8 +30,13 @@ public class Tank {
     private Direction direction;
 
     public Tank(int x, int y, Direction direction) {
+        this(x, y, false, direction);
+    }
+
+    public Tank(int x, int y, boolean enemy, Direction direction) {
         this.x = x;
         this.y = y;
+        this.enemy = enemy;
         this.direction = direction;
     }
 
@@ -70,23 +76,24 @@ public class Tank {
         }
     }
     Image getImage() {
+        String prefix = enemy ? "e" : "";
         switch (direction) {
             case UP:
-                return new ImageIcon("assets/images/tankU.gif").getImage();
+                return new ImageIcon("assets/images/" + prefix + "tankU.gif").getImage();
             case DOWN:
-                return new ImageIcon("assets/images/tankD.gif").getImage();
+                return new ImageIcon("assets/images/" + prefix + "tankD.gif").getImage();
             case LEFT:
-                return new ImageIcon("assets/images/tankL.gif").getImage();
+                return new ImageIcon("assets/images/" + prefix + "tankL.gif").getImage();
             case RIGHT:
-                return new ImageIcon("assets/images/tankR.gif").getImage();
+                return new ImageIcon("assets/images/" + prefix + "tankR.gif").getImage();
             case UPLEFT:
-                return new ImageIcon("assets/images/tankLU.gif").getImage();
+                return new ImageIcon("assets/images/" +prefix + "tankLU.gif").getImage();
             case UPRIGT:
-                return new ImageIcon("assets/images/tankRU.gif").getImage();
+                return new ImageIcon("assets/images/" + prefix + "tankRU.gif").getImage();
             case DOWNLEFT:
-                return new ImageIcon("assets/images/tankLD.gif").getImage();
+                return new ImageIcon("assets/images/" +prefix + "tankLD.gif").getImage();
             case DOWNRIGHT:
-                return new ImageIcon("assets/images/tankRD.gif").getImage();
+                return new ImageIcon("assets/images/" +prefix + "tankRD.gif").getImage();
         }
         return null;
     }
